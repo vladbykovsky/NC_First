03c572966e5ea08d2bb8e3ba109ec45d3e949fb1
vladbykovsky <47810213+vladbykovsky@users.noreply.github.com>
2019-03-18 19:43:33 +0300

    First

---
+++ fapi/src/main/java/com/netcracker/edu/vlad/controllers/SimpleController.java
@@ -0,0 +1,38 @@
+package com.netcracker.edu.vlad.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @RequestMapping(value = "/get-simple-object")
+    SimpleObject getSimpleString()
+    { SimpleObject simpleObject=new SimpleObject();
+        simpleObject.setId("1");
+        simpleObject.setName("name");
+        return simpleObject;
+
+    }
+}
+
+    class SimpleObject {
+        String id;
+        String name;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
